84fce9d2318f979f76ea5374f46f6eee6205accc
yiranlandtour <yiranlandtour@163.com>
2024-01-23 09:51:43 +0000

    reset

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,7 @@ const App = () => {
         </div>
       </div>
     </div>
-          {/* <WalletMultiButton /> */}
+          <WalletMultiButton />
         </WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
